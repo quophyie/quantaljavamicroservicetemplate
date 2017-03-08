cbfe533a794db10c220987d76cfd54e750aba1ed
quophyie <quophyie@yahoo.com>
2017-03-08 22:59:05 +0000

    Added Mockito Test runner and  mockito imports

--- src/test/java/com/quantal/quantalmicroservicetemplate/MicroserviceApplicationTests.java
+++ src/test/java/com/quantal/quantalmicroservicetemplate/MicroserviceApplicationTests.java
@@ -5,7 +5,23 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class MicroserviceApplicationTests {
 
