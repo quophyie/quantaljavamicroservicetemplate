42877509d58c478444bfc76851ee4f13e1982dac
quophyie <quophyie@yahoo.com>
2018-03-31 03:43:32 +0100

    Updated okHttpClient bean config to auto add X-TraceId Header and X-Event Header

--- src/main/java/com/quantal/quantalmicroservicetemplate/config/api/ApiConfig.java
+++ src/main/java/com/quantal/quantalmicroservicetemplate/config/api/ApiConfig.java
@@ -1,7 +1,13 @@
 package com.quantal.quantalmicroservicetemplate.config.api;
 
 import com.quantal.javashared.aspects.RetrofitRequiredHeadersEnforcerAspectJAspect;
+import com.quantal.javashared.constants.CommonConstants;
+import com.quantal.javashared.dto.LogEvent;
+import com.quantal.javashared.dto.LoggerConfig;
+import com.quantal.javashared.logger.QuantalLogger;
+import com.quantal.javashared.logger.QuantalLoggerFactory;
 import com.quantal.quantalmicroservicetemplate.services.api.GiphyApiService;
+import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -10,12 +16,25 @@ import okio.Buffer;
 import org.aspectj.lang.Aspects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import retrofit2.Retrofit;
 import retrofit2.adapter.java8.Java8CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+import static com.quantal.javashared.constants.CommonConstants.EVENT_HEADER_KEY;
+import static com.quantal.javashared.constants.CommonConstants.EVENT_KEY;
+import static com.quantal.javashared.constants.CommonConstants.TRACE_ID_HEADER_KEY;
+import static com.quantal.javashared.constants.CommonConstants.TRACE_ID_MDC_KEY;
 
 /**
  * Created by dman on 14/03/2017.
@@ -26,29 +45,91 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 @Configuration
 public class ApiConfig
 {
+    private final Environment env;
+    private QuantalLogger logger;
+
+    @Autowired
+    public ApiConfig(Environment env, LoggerConfig loggerConfig){
+        this.env = env;
+        logger = QuantalLoggerFactory.getLogzioLogger(ApiConfig.class, loggerConfig);
+    }
+
+    private String getRequestBody(Request request, Buffer buffer ){
+        if (request.body() != null) {
+            try {
+                request.body().writeTo(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return buffer.readUtf8();
+        }
+        return  null;
+    }
 
     @Bean
     public OkHttpClient okHttpClient() {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();;
-        Logger logger = LoggerFactory.getLogger("LoggingInterceptor");
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
 
         builder.interceptors().add(chain -> {
-            Request request = chain.request();
-            final Buffer buffer = new Buffer();
-            request.body().writeTo(buffer);
-            String requestBody = buffer.readUtf8();
-            long t1 = System.nanoTime();
-            logger.info(String.format("Sending request %s on %s%n%s %s",
-                    request.url(), chain.connection(), request.headers(), requestBody));
 
-            Response response = chain.proceed(request);
+            String requestBody = null;
+            Request request = chain.request();
+            Request newRequest;
+
+
+            MDC.put(EVENT_KEY, request.header(EVENT_HEADER_KEY));
+            MDC.put(TRACE_ID_MDC_KEY, request.header(TRACE_ID_HEADER_KEY));
+            LogEvent event = new LogEvent(request.header(CommonConstants.EVENT_HEADER_KEY));
+            final boolean bAllRequiredHeadersFound = request
+                    .headers()
+                    .names()
+                    .stream()
+                    .filter(name -> name.equalsIgnoreCase(EVENT_HEADER_KEY) || name.equalsIgnoreCase(TRACE_ID_HEADER_KEY))
+                    .count() == 2;
+
+            if(!bAllRequiredHeadersFound){
+                String msg = String.format("The required request headers %s and %s were not found. " +
+                                "Please make sure that your api interface method " +
+                                "has headers %s and %s ",
+                        EVENT_HEADER_KEY, TRACE_ID_HEADER_KEY, EVENT_HEADER_KEY, TRACE_ID_HEADER_KEY);
+                RuntimeException exception =  new IllegalStateException(msg);
+                throw exception;
+
+            }
+
+            final Buffer buffer = new Buffer();
+
+            long t1 = System.nanoTime();
+
+            newRequest = request.newBuilder()
+                                .build();
+
+            logger.info(String.format("Sending request for %s",newRequest.url()),
+                    new HashMap<String, Object>() {{
+                        put("requestUrl",newRequest.url().url());
+                        put("requestUrlParts",newRequest.url());
+                        put("connection",chain.connection());
+                        put("headers", newRequest.headers());
+                        put("requestBody", getRequestBody(request, buffer));
+                    }},
+                    newRequest.url(), chain.connection(), newRequest.headers(), requestBody,
+                    event
+            );
+
+            Response response = chain.proceed(newRequest);
 
             long t2 = System.nanoTime();
             String responseBody =response.body().string();
-            logger.info(String.format("Received response for %s in %.1fms%n%s %s",
-                    response.request().url(), (t2 - t1) / 1e6d, response.headers(), responseBody));
 
-            //return response;
+            logger.info(String.format("Received response for %s",
+                    response.request().url()),
+                    new HashMap<String, Object>() {{
+                        put("duration", (t2 - t1) / 1e6d);
+                        put("headers", response.headers());
+                        put("responseBody",responseBody);
+                        put("statusCode",response.code());
+                    }});
+
             return response.newBuilder()
                     .body(ResponseBody.create(response.body().contentType(), responseBody))
                     .build();
