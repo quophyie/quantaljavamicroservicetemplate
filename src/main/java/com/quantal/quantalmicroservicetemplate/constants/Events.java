fd5c50a36042495a50ea0de775e6a9affb7d7626
quophyie <quophyie@yahoo.com>
2018-04-03 21:10:41 +0100

    Added events constants

---
+++ src/main/java/com/quantal/quantalmicroservicetemplate/constants/Events.java
@@ -0,0 +1,10 @@
+package com.quantal.quantalmicroservicetemplate.constants;
+
+public class Events {
+    public static final String USER_CREATE = "USER_CREATE";
+    public static final String USER_DELETE = "USER_DELETE";
+    public static final String USER_PASSWORD_RESET = "USER_PASSWORD_RESET";
+    public static final String USER_PASSWORD_RESET_REQUEST = "USER_PASSWORD_RESET_REQUEST";
+
+    private Events(){}
+}
